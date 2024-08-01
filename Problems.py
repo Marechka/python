595238d94426fd4d97b3126ba88f69c5903c229b
Masha <39933741+Marechka@users.noreply.github.com>
2024-08-01 11:51:58 -0700

    last digit of the factorial

--- Problems.py
+++ Problems.py
@@ -183,3 +183,14 @@ def countPrimes(self, n: int) -> int:
                     ref[j]=False
             i+=1
         return ref.count(True)-2
+
+# last digit of the factorial
+n = int(input())
+
+if n == 0 or n == 1:
+   print(1)
+else:
+   result = 1
+   for i in range(1,n+1):
+      result *= i
+   print(result%10)
