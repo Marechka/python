e859b71abea9bdc9ccfcf75087e2aff5a9e847f9
Masha <39933741+Marechka@users.noreply.github.com>
2024-08-01 12:01:59 -0700

    trailing 0s approach after n > 5

--- Problems.py
+++ Problems.py
@@ -194,3 +194,16 @@ else:
    for i in range(1,n+1):
       result *= i
    print(result%10)
+
+# trailing 0s approach after n > 5
+n = int(input())
+
+result = 1
+if n < 6:
+   
+   for i in range(1, n+1):
+      result *= i
+   result %= 10
+else:
+   result = 0
+print(result)
