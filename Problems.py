c9af64746c561c484421456a470d7297858a2e6d
Masha <39933741+Marechka@users.noreply.github.com>
2024-08-01 15:36:59 -0700

    #trailing 0s in factorial

--- Problems.py
+++ Problems.py
@@ -228,3 +228,14 @@ if n != 0 or n != 1:
    while result%10 == 0:
       result = result//10
 print(result%10)
+
+#trailing 0s in factorial
+def trailingZeroes(self, n: int) -> int:
+   zero_count = 0
+   for i in range(5, n + 1, 5):
+      current = i
+      while current % 5 == 0:
+         zero_count += 1
+         current //= 5
+
+   return zero_count
