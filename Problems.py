36d07975a2f55b85a7cccd7eeb7d1c7098158a2f
Masha <39933741+Marechka@users.noreply.github.com>
2024-09-07 21:47:09 -0700

    zip 2 sorted arrays

--- Problems.py
+++ Problems.py
@@ -500,3 +500,21 @@ def check_for_target(nums, target):
          left += 1
    return False
       
+# zip 2 sorted arrays
+def combine(arr1, arr2):
+   i = j = 0
+   ans = []
+   while i < len(arr1) and j < len(arr2):
+      if arr1[i] < arr2[j]:
+         ans.append(arr1[i])
+         i += 1
+      else:
+         ans.append(arr2[j])
+         j += 1
+   while i < len(arr1):
+      ans.append(arr1[i])
+      i += 1
+   while j < len(arr2):
+      ans.append(arr2[j])
+      j += 1
+   return ans
