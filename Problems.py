a9b747879b4855467fe7034bf6ed823944ae8a96
Masha <39933741+Marechka@users.noreply.github.com>
2024-09-14 11:57:13 -0700

    prefix problem

--- Problems.py
+++ Problems.py
@@ -616,3 +616,18 @@ def longestOnes(nums,k):
          left += 1
       ans = max(ans, right-left+1)
    return ans
+
+#Given an integer array nums, an array queries where queries[i] = [x, y] 
+# and an integer limit, return a boolean array that represents the answer 
+# to each query. A query is true if the sum of the subarray from x to y is 
+# less than limit, or false otherwise.
+
+def subarray_sums(queries, nums, k):
+   prefix = [nums[0]]
+   for i in range(1, len(nums)):
+      prefix.append(nums[i]+prefix[-1])
+   
+   ans = []
+   for x, y in queries:
+      ans.append(prefix[y]-prefix[x] + nums[x] < k)
+   return ans
