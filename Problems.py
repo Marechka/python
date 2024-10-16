77993450b0b193de21fb454ccf2eaef313f5ab6b
Masha <39933741+Marechka@users.noreply.github.com>
2024-10-16 15:51:31 -0700

    two sum

--- Problems.py
+++ Problems.py
@@ -782,3 +782,16 @@ def maxVowels(self, s: str, k: int) -> int:
             left += 1
             max_vowel = max(max_vowel, curr_vowel)
         return max_vowel
+
+# twoSum
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic = {}
+        for i in range(len(nums)):
+            num = nums[i]
+            complement = target - num
+            if complement in dic: # This operation is O(1)!
+                return [i, dic[complement]]
+            
+            dic[num] = i
+        
+        return [-1, -1]
