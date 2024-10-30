fc2004cfe70379f2f1ddbaeab9b4add44871f8d0
Masha <39933741+Marechka@users.noreply.github.com>
2024-10-30 11:05:45 -0700

    grouping anagrams

--- Problems.py
+++ Problems.py
@@ -899,3 +899,11 @@ def findMaxLength(self,nums):
                 arr[count + len(nums)] = i
         
         return maxlen    
+# grouping anagrams 
+def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = defaultdict(list)
+        for s in strs:
+            key = "".join(sorted(s))
+            groups[key].append(s)
+        
+        return list(groups.values())
